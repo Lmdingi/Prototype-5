fae85000d3533091deaede79f6c95b9906e051cd
Lmdingi <67228004+Lmdingi@users.noreply.github.com>
2022-09-14 21:37:56 +0200

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.5f;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    private int score;
+    public bool isGameActive;
+    public GameObject titleScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void StartGame(int difficulty) 
+    {
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
+        spawnRate /= difficulty;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+    public void UpdateScore(int scoreToAdd)
+    {
+        
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+     public void GameOver()
+     {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+     }
+     public void RestartGame()
+     {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+     }
+}
